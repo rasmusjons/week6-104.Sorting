45e53cca790a55c21f77d01b88bfc003fc3acc54
Rasmus Jonsson <rasmusjons@gmail.com>
2019-10-30 22:24:30 +0100

    104.2 - done!

--- Main.java
+++ Main.java
@@ -1,7 +1,42 @@
 
 public class Main {
+
     public static void main(String[] args) {
         // write testcode here
+
+        // indexes:   0  1  2  3  4
+        int[] values = {6, 5, 8, 7, 11};
+        System.out.println("Index of the smallest: " + indexOfTheSmallest(values));
+
+    }
+
+    public static int smallest(int[] array) {
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (smallest > array[i]) {
+                smallest = array[i];
+            }
+
+        }
+        return smallest;
+
+    }
+
+    public static int indexOfTheSmallest(int[] array) {
+        // code goes here
+
+        int smallestNumber = smallest(array);
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (smallestNumber == array[i]) {
+                index = i;
+            }
+
+        }
+        return index;
     }
 
 }
