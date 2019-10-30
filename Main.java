c1766a2a01ad927a1e20baf57e67b18745c55c86
Rasmus Jonsson <rasmusjons@gmail.com>
2019-10-30 23:14:56 +0100

    done!

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
-
-        // indexes:   0  1  2  3  4
-        int[] values = {6, 5, 8, 7, 11};
-        System.out.println("Index of the smallest: " + indexOfTheSmallest(values));
-
+        int[] values = {8, 3, 7, 9, 1, 2, 4};
+        sort(values);
+        System.out.println(Arrays.toString(values));
+        
     }
 
     public static int smallest(int[] array) {
@@ -28,15 +28,52 @@ public class Main {
         // code goes here
 
         int smallestNumber = smallest(array);
-        int index = 0;
+        int indexSmallest = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (smallestNumber == array[i]) {
-                index = i;
+                indexSmallest = i;
             }
 
         }
-        return index;
+        return indexSmallest;
+    }
+
+    public static int indexOfTheSmallestStartingFrom(int[] array, int index) {
+
+        int indexSmallest = 99;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = (0 + index); i < array.length; i++) {
+            if (smallest > array[i]) {              // återanvänder koden från smallest()
+                smallest = array[i];                // 
+            }
+            if (smallest == array[i]) {
+                indexSmallest = i;
+            }
+
+        }
+        return indexSmallest;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+
+        int valueOne = array[index1];
+        int valueTwo = array[index2];
+
+        array[index1] = valueTwo;
+        array[index2] = valueOne;
+
+    }
+
+    public static void sort(int[] array) {
+                
+        for (int i = 0; i < array.length; i++){
+            int smallest = indexOfTheSmallestStartingFrom(array,i);
+        swap(array, i, smallest);
+        }
+        
+
     }
 
 }
